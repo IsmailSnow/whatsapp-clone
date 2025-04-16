0059d46b9d5a3cf571bf36617243fb2f8e0e418c
Ismail <salmi.ismail8u@gmail.com>
2025-04-16 14:02:47 +0100

    ⚡️ app start

---
+++ whatsappsclone/src/main/java/com/example/whatsappsclone/WhatsappscloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsappsclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappscloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappscloneApplication.class, args);
+    }
+
+}
---
+++ whatsappsclone/src/test/java/com/example/whatsappsclone/WhatsappscloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappsclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappscloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
